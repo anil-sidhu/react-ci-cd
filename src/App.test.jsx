3a1e88ad72a851b25ae960a0fdd2b76e576b5a55
Apple <Apple@MacBook-Pro.local>
2026-07-21 01:21:34 +0530

    deployment code push

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 describe('App Component', () => {
-  it('renders hello message', () => {
+  it('renders hello message test', () => {
     render(<App />)
 
     expect(screen.getByText(/hello anil sidhu/i)).not.toBeInTheDocument()
