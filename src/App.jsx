7e545d0eef10040fac7ffc2d6cb651f2918cc5b5
Apple <Apple@MacBook-Pro.local>
2026-07-24 23:51:35 +0530

    worked on app.jsx for  lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
      <div>
@@ -11,3 +10,4 @@ function App() {
 }
 
 export default App
+ 
